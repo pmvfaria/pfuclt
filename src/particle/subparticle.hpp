1b105fb5752a00f042a5a10a8745f84fe627e739
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-08-02 16:32:31 +0100

    simplify SubParticle definition

--- src/particle/subparticle.hpp
+++ src/particle/subparticle.hpp
@@ -6,41 +6,47 @@
 
 namespace pfuclt::particle{
 
-class RobotSubParticle{
- private:
-  std::array<double, 3> p_{0.0, 0.0, 0.0};
+template <auto STATES>
+class SubParticle{
+ protected:
+  std::array<double, STATES> p_;
 
  public:
-  RobotSubParticle() = default;
-  explicit RobotSubParticle(double init_value) { p_.fill(init_value); }
-  explicit RobotSubParticle(double init_values[3]) { std::copy(p_.begin(), p_.end(), init_values); }
-  RobotSubParticle(double x, double y, double theta) : p_{x,y,theta} { };
+  SubParticle() = default;
+  explicit SubParticle(double init_value) { p_.fill(init_value); }
+  SubParticle(std::initializer_list<double> init_values) { }
 
-//  std::array<double, 3> operator(){ return p_; };
+  std::array<double, STATES>& operator()() noexcept { return p_; }
+};
+
+class RobotSubParticle: public SubParticle<3>{
+  // Inherit constructors
+  using SubParticle::SubParticle;
+
+ public:
+  RobotSubParticle(double x, double y, double theta) : SubParticle({x, y, theta}) { };
+
   const double x() const { return p_[0]; };
   const double y() const { return p_[1]; };
   const double theta() const { return p_[2]; };
-  double x() { return p_[0]; };
-  double y() { return p_[0]; };
-  double theta() { return p_[0]; };
+  double& x() { return p_[0]; };
+  double& y() { return p_[1]; };
+  double& theta() { return p_[2]; };
 };
 
-class TargetSubParticle {
- private:
-  std::array<double, 3> p_{0,0,0};
+class TargetSubParticle: public SubParticle<3>{
+  // Inherit constructors
+  using SubParticle::SubParticle;
 
  public:
-  TargetSubParticle() = default;
-  explicit TargetSubParticle(double init_value) { p_.fill(init_value); }
-  explicit TargetSubParticle(double init_values[3]) { std::copy(p_.begin(), p_.end(), init_values); }
-  TargetSubParticle(double x, double y, double z) : p_{x,y,z} { };
+  TargetSubParticle(double x, double y, double z) : SubParticle({x, y, z}) { };
 
-  inline const double x() const { return p_[0]; };
-  inline const double y() const { return p_[1]; };
-  inline const double z() const { return p_[2]; };
-  inline double x() { return p_[0]; };
-  inline double y() { return p_[1]; };
-  inline double z() { return p_[2]; };
+  const double x() const { return p_[0]; };
+  const double y() const { return p_[1]; };
+  const double theta() const { return p_[2]; };
+  double& x() { return p_[0]; };
+  double& y() { return p_[1]; };
+  double& theta() { return p_[2]; };
 };
 
 typedef std::vector<RobotSubParticle> RobotSubParticles;
