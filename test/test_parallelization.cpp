c39e39eb11e591be8523c89c82d41a04381cf0f9
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-08-02 10:45:01 +0100

    test parallelization results in milliseconds

--- test/test_parallelization.cpp
+++ test/test_parallelization.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv){
 
-  ::pfuclt::particle::Particles particles(100000, 5, 2);
+  ::pfuclt::particle::Particles particles(10000, 5, 2);
 
   std::cout << "Warming up..." << std::endl;
 
@@ -15,22 +15,22 @@ int main(int argc, char **argv){
   std::cout << "Warmed up!" << std::endl;
 
   StopWatch watch;
-  for(uint i=0; i<10000; ++i)
+  for(uint i=0; i<100000; ++i)
     particles.normalizeWeights();
 
-  std::cout << "Without parallel library - took " << watch.ElapsedSec() << " seconds" << std::endl;
+  std::cout << "Without parallel library - took " << watch.ElapsedMs() << " ms" << std::endl;
 
   watch.Restart();
   for(uint i=0; i<10000; ++i)
     particles.normalizeWeights(__gnu_parallel::parallel_balanced);
 
-  std::cout << "Parallel (balanced) - took " << watch.ElapsedSec() << " seconds" << std::endl;
+  std::cout << "Parallel (balanced) - took " << watch.ElapsedMs() << " ms" << std::endl;
 
   watch.Restart();
   for(uint i=0; i<10000; ++i)
     particles.normalizeWeights(__gnu_parallel::parallel_unbalanced);
 
-  std::cout << "Parallel (unbalanced) - took " << watch.ElapsedSec() << " seconds" << std::endl;
+  std::cout << "Parallel (unbalanced) - took " << watch.ElapsedMs() << " ms" << std::endl;
 
   return EXIT_SUCCESS;
 }
