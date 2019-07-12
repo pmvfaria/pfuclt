eda5b4e5162c2eee2fb8a2283f1a384d894f2527
Pedro Faria <pedro.m.faria@tecnico.ulisboa.pt>
2019-07-12 11:43:57 +0100

    Target Measurements

--- src/sensor/target_data.cpp
+++ src/sensor/target_data.cpp
@@ -2,10 +2,21 @@
 
 namespace pfuclt::sensor::target_data{
 
-TargetMeasurement fromRosMsg(const clt_msgs::MeasurementStampedConstPtr &msg){
-  ROS_ASSERT_MSG(msg->measurement.type == clt_msgs::Measurement::TARGET_RANGE_BEARING, "Only TARGET_RANGE_BEARING is supported");
-  return TargetMeasurement{msg->header.stamp, msg->measurement.id,
-                            msg->measurement.range, msg->measurement.bearing, msg->measurement.noise};
+TargetLikelihood uncertaintyModel(const TargetMeasurement& m) {
+  
+}
+
+std::unique_ptr<TargetMeasurements> fromRosMsg(const clt_msgs::MeasurementArrayConstPtr &msg){
+
+  auto ts = std::make_unique<TargetMeasurements> (msg->header.frame_id, msg->header.stamp, msg->measurements.size());
+
+  for(const auto& m: msg->measurements){
+    ROS_ASSERT_MSG(m.type == clt_msgs::Measurement::TARGET_RANGE_BEARING, "Only TARGET_RANGE_BEARING is supported");
+    bool seen = (m.status == clt_msgs::Measurement::PARTIAL || m.status == clt_msgs::Measurement::SEEN);
+    ts->measurements.emplace_back(TargetMeasurement{m.id, seen, m.range, m.bearing});
+  }
+
+  return ts;
 }
 
 } // namespace pfuclt::sensor::target_data
--- src/sensor/target_data.hpp
+++ src/sensor/target_data.hpp
@@ -5,22 +5,43 @@
 
 #include <vector>
 #include <ros/ros.h>
-#include <clt_msgs/MeasurementStamped.h>
+#include <clt_msgs/MeasurementArray.h>
 
 namespace pfuclt::sensor::target_data{
 
-struct TargetMeasurement{
-  ros::Time stamp;
-  uint16_t id;
-  double range, bearing, noise;
+inline double K_range;
+inline double K_bearing;
+
+struct TargetLikelihood{
+  double dd, pp;
 };
+
+struct TargetMeasurement{
+  uint16_t id;
+  bool seen;
+  double range, bearing;
+};
+
+struct TargetMeasurements{
+  std::string frame;
+  ros::Time stamp;
+
+  std::vector<TargetMeasurement> measurements;
+
+  TargetMeasurements(const std::string &frame_a, const ros::Time& stamp_a, const std::size_t number_measurements)
+  : frame(frame_a), stamp(stamp_a) {
+    measurements.reserve(number_measurements);
+  }
+};
+
+TargetLikelihood uncertaintyModel(const TargetMeasurement&);
 
 /**
  * @brief Retrieves target measurement from ROS message
  * @param msg Message which contains the target measurement
  * @return Struct TargetMeasurement containing the measurement
  */
-TargetMeasurement fromRosMsg(const clt_msgs::MeasurementStampedConstPtr &msg);
+std::unique_ptr<TargetMeasurements> fromRosMsg(const clt_msgs::MeasurementArrayConstPtr &msg);
 
 } // namespace pfuclt::sensor::target_data
 
